c35b982689738c7ea3d07a305ce0097dcf56fd62
yoroih <roycristhopherqp@gmail.com>
2024-05-06 10:21:38 -0500

    alarm finish

--- code/2alarm.js
+++ code/2alarm.js
@@ -8,7 +8,7 @@ export function digitalClock(clock, btnPlay, btnStop) {
       clockTempo = setInterval(() => {
         let clockHour = new Date().toLocaleTimeString();
         d.querySelector(clock).innerHTML = `<h3>${clockHour}</h3>`;
-      }, 1000);
+      }, 0);
 
       e.target.disabled = true;
     };
@@ -18,10 +18,29 @@ export function digitalClock(clock, btnPlay, btnStop) {
       d.querySelector(clock).innerHTML = null;
       d.querySelector(btnPlay).disabled = false;
     };
-
   })
 };
 
-export function alarm() {
+export function alarm(sound, btnPlay, btnStop) {
+  let alarmTempo;
+  const $alarm = d.createElement("audio");
+  $alarm.src = sound;
 
+  d.addEventListener("click", e => {
+    if(e.target.matches(btnPlay)){
+      alarmTempo = setTimeout(() => {
+        $alarm.play();
+      }, 50);
+    
+    e.target.disabled = true;
+    };
+
+    if(e.target.matches(btnStop)){
+      clearTimeout(alarmTempo);
+      $alarm.pause();
+      $alarm.currentTime = 0;
+      d.querySelector(btnPlay).disabled = false;
+    }
+
+  });
 }
--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburguerMenu(".panel-btn", ".panel", ".menu a");
 
-  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+
+  alarm("assets/alarm.mp3", "#activar-alarma", "#desactivar-alarma");
 
 });
+
