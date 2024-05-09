bc0f7016cf219f82b36f22af800a147d10a19cc8
yoroih <roycristhopherqp@gmail.com>
2024-05-09 12:16:39 -0500

    webcam detection finished

--- code/11deteccion_webcam.js
+++ code/11deteccion_webcam.js
@@ -2,5 +2,22 @@ const d = document;
 const n = navigator;
 
 export default function webCam(id){
-  
+  const $video = d.getElementById(id);
+
+  if(n.mediaDevices.getUserMedia){
+    n.mediaDevices.getUserMedia({
+      video: true, audio: true
+    }).then(
+      (stream) => {
+        $video.srcObject = stream;
+        $video.play();
+      }
+    ).catch(
+      (err) => {
+        $video.insertAdjacentHTML("beforebegin",`<p><mark><b>Error: </b>${err}</mark></p>`);
+        console.log(`Error: ${err}`);
+      }
+    )
+  }
+
 };
