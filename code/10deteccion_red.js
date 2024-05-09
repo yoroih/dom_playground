1d2f6b963998f02937b95ff4d0d04e0fd73e533f
yoroih <roycristhopherqp@gmail.com>
2024-05-08 22:31:55 -0500

    red detection

--- code/10deteccion_red.js
+++ code/10deteccion_red.js
@@ -3,5 +3,6 @@ const w = window;
 const n = navigator;
 
 export default function networkStatus(){
-  
+  w.addEventListener("online", e => console.log(n.onLine));
+  w.addEventListener("offline", e => console.log(n.onLine));
 };
