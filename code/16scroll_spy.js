fea3992ef983d18bce08b6555954d17e5d77d785
yoroih <roycristhopherqp@gmail.com>
2024-05-12 21:41:26 -0500

    je

--- code/16scroll_spy.js
+++ code/16scroll_spy.js
@@ -1,5 +1,15 @@
 const d = document;
 
 export default function scrollSpy() {
+  const $section = d.querySelectorAll("section[data-scroll-spy]");
   
+  const cb = (entries) => {
+    console.log("entries", entries)
+  }
+
+  const observer = new IntersectionObserver(cb, {});
+
+  $section.forEach(el => observer.observe(el));
 };
+
+
