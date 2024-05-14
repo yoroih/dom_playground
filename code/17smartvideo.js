08de1754882907fa9f59cdf314d27a61a37662a0
yoroih <roycristhopherqp@gmail.com>
2024-05-13 21:30:08 -0500

    some fixes

--- code/17smartvideo.js
+++ code/17smartvideo.js
@@ -1,3 +1,20 @@
-export default function smartVideo(){
+const d = document;
+const w = window;
 
+export default function smartVideo(){
+  const $videos = d.querySelectorAll("video[smart-data-video]");
+
+  const cb = entries => {
+    entries.forEach(entry => {
+      if(entry.isIntersecting){
+        entry.target.play();
+      }else{
+        entry.target.pause();
+      }
+    });
+  }
+
+  const observer = new IntersectionObserver(cb, {threshold: 0.5});
+
+  $videos.forEach(el => observer.observe(el));
 };
